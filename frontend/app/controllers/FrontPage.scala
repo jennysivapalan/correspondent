652236a429b4c4a4fb22c8354155d2d0cd7ecfc2
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-07-15 11:42:41 +0100

    Key events added

--- frontend/app/controllers/FrontPage.scala
+++ frontend/app/controllers/FrontPage.scala
@@ -5,7 +5,7 @@ import configuration.Config
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.ws.WS
 import play.api.Play.current
-import model.Bundle
+import model.{KeyEvent, Bundle}
 import play.api.libs.json._
 
 
@@ -19,7 +19,16 @@ trait FrontPage extends Controller {
         val images = (mainBlock \ "assets").as[Seq[JsValue]]
 
         val image = images.filter( image => (image \ "fields" \ "width").asOpt[String].exists( _ == "620"))
-        val bundle = new Bundle(Json.stringify(headline), Json.stringify(image(0) \ "url"), Nil, Nil)
+
+        val blocks = (json \ "blocks" \ "data").as[Seq[JsValue]]
+        val keyEvents = blocks.map { block =>
+
+          val id = Json.stringify(block \ "data" \ "id")
+          val title =(block \ "data" \ "attributes" \ "title").asOpt[String]
+          new KeyEvent(id, title)
+
+        }
+        val bundle = new Bundle(Json.stringify(headline), Json.stringify(image(0) \ "url"), keyEvents.toList, Nil)
 
         Ok(views.html.index(bundle))
 
--- frontend/app/model/Bundle.scala
+++ frontend/app/model/Bundle.scala
@@ -3,6 +3,6 @@ package model
 
 case class Bundle(heading:String, mainImage: String, keyEvents: List[KeyEvent], blocks:  List[Block])
 
-case class KeyEvent(id: String, title:String)
+case class KeyEvent(id: String, title:Option[String])
 
 case class Block(id:String, elementType: String, html: String, originalUrl: Option[String])
