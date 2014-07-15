60a5d98308657982460ba8aa0206a5574096e05a
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-07-15 11:20:38 +0100

    heading and image

--- frontend/app/controllers/FrontPage.scala
+++ frontend/app/controllers/FrontPage.scala
@@ -5,21 +5,30 @@ import configuration.Config
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.ws.WS
 import play.api.Play.current
+import model.Bundle
+import play.api.libs.json._
 
 
 trait FrontPage extends Controller {
 
-
-  def index = Action.async {
+    def index = Action.async { implicit request =>
       WS.url(getBundle()).get().map { response =>
-        Ok(response.json \ "data" \ "blocks") //todo
+        val json = (response.json) \ "data"
+        val headline = (json \ "fields" \ "headline" \ "data" )
+        val images = (json \ "thumbnail" \ "data" \ "assets").as[Seq[JsValue]]
+        println(images(6) \ "fields" \ "width")
 
+        val image = images.filter( image => (image \ "fields" \ "width").asOpt[String].exists( _ == "540"))
+        println(image)
+        val bundle = new Bundle(Json.stringify(headline), Json.stringify(image(0) \ "url"), Nil, Nil)
 
-    }
+        Ok(views.html.index(bundle))
 
+      }
   }
+
   private def getBundle(): String = {
-    "http://%s:9081/api/content/53c3feb83004f2a0164537b9/live".format(Config.composer) //todo add in a config
+    "http://%s:9081/api/content/53c3feb83004f2a0164537b9/live".format(Config.composer)
   }
 
 }
